84b32fbb7ca5bd452e0390fda119acbe17dc5d00
iamogir <ia.mogirevsky@gmail.com>
2026-03-10 22:03:56 +0200

    added drafts of footer and header

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,7 +4,8 @@ import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
+
     <BrowserRouter>
-        <App />,
+        <App />
     </BrowserRouter>
 )
