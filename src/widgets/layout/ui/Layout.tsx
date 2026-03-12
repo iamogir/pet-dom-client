34bec5e5e00ff64beb9f61a56052ef379b486953
iamogir <ia.mogirevsky@gmail.com>
2026-03-12 22:47:13 +0200

    drafts for navigation mobile and sidebar

--- src/widgets/layout/ui/Layout.tsx
+++ src/widgets/layout/ui/Layout.tsx
@@ -2,16 +2,24 @@ import style from './layout.module.css'
 import {Header} from "widgets/header";
 import {Footer} from "widgets/footer";
 import {Outlet} from "react-router-dom";
+import {Sidebar} from "widgets/sidebar";
+import {Navigation} from "widgets/navigation";
 
 export const Layout = () => {
     return (
         <div className={style.layout}>
             layout
-            {/*sidebar*/}
+            <div>
+                <Sidebar/>
+            </div>
+
             <header><Header/></header>
             <main><Outlet/></main>
-            {/*bottom nav*/}
             <footer><Footer/></footer>
+
+            <div className={style.hide}>
+                <Navigation/>
+            </div>
         </div>
     );
 };
