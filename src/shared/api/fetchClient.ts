aac4dc7ba544b7bd8a3256474530e454057ae464
iamogir <ia.mogirevsky@gmail.com>
2026-03-13 21:20:16 +0200

    created fetch client

---
+++ src/shared/api/fetchClient.ts
@@ -0,0 +1,18 @@
+import {BASE_URL} from "shared/const";
+
+export const fetchClient: <T>(endpoint: string, options?: RequestInit) => Promise<T> =
+    async (endpoint, options?)=> {
+    const response = await fetch(BASE_URL + endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            ...options?.headers,
+        },
+        ...options,
+    })
+    if (!response.ok) {
+        const error = await response.text()
+        throw new Error(error)
+    }
+
+    return response.json();
+}
