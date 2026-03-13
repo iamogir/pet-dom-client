f531db6b2169532f0dcfe06c3d426c8b373fd549
iamogir <ia.mogirevsky@gmail.com>
2026-03-13 20:10:42 +0200

    added mock handler

---
+++ src/mocks/handlers.js
@@ -0,0 +1,40 @@
+import {http, HttpResponse} from "msw";
+
+export const handlers = [
+    http.get('/api/pets', () => {
+        return HttpResponse.json(
+            {
+                data: [ {
+                    id: '0',
+                    species: 'dog',
+                    breed: 'corgi',
+                    birthDate: Date.now(),
+                    weight: 12,
+                    sex: 'male',
+                    photoUrl: null,
+                },
+                {
+                    id: '1',
+                    species: 'cat',
+                    breed: 'none',
+                    birthDate: Date.now(),
+                    weight: 5,
+                    sex: 'female',
+                    photoUrl: null,
+                },
+                {
+                    id: '2',
+                    species: 'dog',
+                    breed: 'corgi',
+                    birthDate: Date.now(),
+                    weight: 10,
+                    sex: 'female',
+                    photoUrl: null,
+                } ],
+                meta: {
+                    total: 3
+                }
+            }
+        )
+    })
+]
