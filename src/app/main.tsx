835a7cb78b09a62d68a0dbdd5028325e525c7c63
iamogir <ia.mogirevsky@gmail.com>
2026-03-13 20:46:32 +0200

    added mock and enabled mocking

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from 'app/App.tsx'
 import {QueryProvider} from "app/providers/QueryProvider.tsx";
+import {worker} from '../mocks/browser.ts';
+
+const enableMocking = async () => {
+    if (import.meta.env.DEV) {
+        await worker.start()
+    }
+}
+
+enableMocking();
 
 createRoot(document.getElementById('root')!).render(
         <QueryProvider>
