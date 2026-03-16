c1cccd72b2b2783456f1eb609237c0c2ef6f99a7
iamogir <ia.mogirevsky@gmail.com>
2026-03-16 21:30:27 +0200

    profile draft

--- src/pages/profilePage/ui/ProfilePage.tsx
+++ src/pages/profilePage/ui/ProfilePage.tsx
@@ -1,8 +1,21 @@
 import {Link} from "react-router-dom";
+import {useAllUsers} from "entities/user/hooks";
 
 export const ProfilePage = () => {
+
+    const { isLoading, data, error } = useAllUsers();
+
     return (
         <div>
+            {isLoading ? <p>Loading...</p> :
+                (error ? <p>{error.message}</p> :
+                    <div>
+                        {data?.data.map(u => <UserCard key={u.id} user={u} />)}
+                    </div>
+                )
+            }
+
+
             <Link to='/home'>to home</Link>
         </div>
     );
