235edcc4c85a67b17b8fcdab62b48f7310ab26c0
iamogir <ia.mogirevsky@gmail.com>
2026-03-17 23:34:24 +0200

    handler pushed

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import {setupWorker} from "msw/browser";
 import {petHandlers} from "src/mocks//handlers/petHandlers";
 import {userHandlers} from "src/mocks/handlers/userHandlers";
+import {petOwnerHandlers} from "src/mocks/handlers";
 
-export const worker = setupWorker(...petHandlers, ...userHandlers);
+export const worker = setupWorker(...petHandlers, ...userHandlers, ...petOwnerHandlers);
