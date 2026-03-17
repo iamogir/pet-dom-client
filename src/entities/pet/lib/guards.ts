1f5d95a31b9b8c3510b5c380ceb978584807d4c2
iamogir <ia.mogirevsky@gmail.com>
2026-03-17 22:10:39 +0200

    fixed guard

--- src/entities/pet/lib/guards.ts
+++ src/entities/pet/lib/guards.ts
@@ -1,5 +1,5 @@
 import {petSpecies, type PetSpecies} from "entities/pet/model";
 
-export const assertPetSpecies = (species: string): asserts species is PetSpecies => {
-    if (!(petSpecies.includes(species))) throw new Error('invalid species!!!');
+export const assertPetSpecies: (species: string) => void = (species: string): asserts species is PetSpecies => {
+    if (!(petSpecies.includes(species as PetSpecies))) throw new Error('invalid species!!!');
 }
--- src/entities/pet/model/consts.ts
+++ src/entities/pet/model/consts.ts
@@ -1,10 +1,10 @@
-export const petSex = [ 'male',  'female' ];
+export const petSex = [ 'male',  'female' ] as const;
 export type PetSex = typeof petSex[number];
 
-export const petSpecies = [ 'cat', 'dog', 'bird', 'rabbit' ];
+export const petSpecies = [ 'cat', 'dog', 'bird', 'rabbit' ] as const;
 export type PetSpecies = typeof petSpecies[number];
 
-export const petBreed = [ 'beagle', 'corgi', 'jack', 'spaniel' ];
+export const petBreed = [ 'beagle', 'corgi', 'jack', 'spaniel' ] as const;
 export type PetBreed = typeof petBreed[number];
 
 export const imagePlaceholder = 'https://png.pngtree.com/png-clipart/20200401/original/pngtree-cat-one-line-drawing-vector-illustration-minimalism-style-png-image_5330621.jpg';
