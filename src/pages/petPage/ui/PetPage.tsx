b760b67d2361aec3d29f1da0ed46fbd3fbad8ee0
iamogir <ia.mogirevsky@gmail.com>
2026-03-18 21:37:53 +0200

    draft of viewing users of pet

--- src/pages/petPage/ui/PetPage.tsx
+++ src/pages/petPage/ui/PetPage.tsx
@@ -1,8 +1,21 @@
 import {Link} from "react-router-dom";
+import {useAllUsersByPetId} from "entities/petOwner/hooks";
+import {UserCard} from "entities/user/ui/userCard";
 
 export const PetPage = () => {
+
+    const { isLoading, error, data } = useAllUsersByPetId('0');
+
     return (
         <div>
+            {isLoading ? <p>one second...</p> :
+                <div>
+                    <h2>all users of selected pet:</h2>
+                    { error ? <p>{error.message}</p> :
+                            data?.data.map(u => <UserCard key={u.id} user={u} />)
+                    }
+                </div>
+            }
             <Link to='/home'>to home</Link>
         </div>
     );
