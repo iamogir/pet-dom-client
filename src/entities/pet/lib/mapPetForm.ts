0ab89b0b4495552fcb2db5950f82ce23333c1221
iamogir <ia.mogirevsky@gmail.com>
2026-03-20 17:05:34 +0200

    mapper for pet form

--- src/entities/pet/lib/mapPetForm.ts
+++ src/entities/pet/lib/mapPetForm.ts
@@ -1,7 +1,17 @@
+import type {IPetForm} from "entities/pet/model";
+import type {ICreatePetDto} from "entities/pet/model/types.ts";
 
 
-// export const toServerPetObject = (obj: IPet): IPetForm => {
-//     return {
-//
-//     }
-// }
+export const toServerPetObject = (obj: IPetForm): ICreatePetDto => {
+    const newObj: ICreatePetDto = {
+        name: obj.name,
+        species: obj.species,
+        breed: obj.breed,
+        birthDate: obj.birthDate,
+        weight: obj.weight,
+        sex: obj.sex
+    }
+
+    if (obj.photoUrl) newObj.photoUrl = obj.photoUrl;
+    return newObj;
+}
