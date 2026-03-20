da2375b97be5bdc58cb61bdd14772b98f8bf493e
iamogir <ia.mogirevsky@gmail.com>
2026-03-20 21:53:01 +0200

    post request add pet

--- src/mocks/handlers/petHandlers.ts
+++ src/mocks/handlers/petHandlers.ts
@@ -1,5 +1,6 @@
 import {http, HttpResponse} from "msw";
 import {allPets} from "src/mocks/data";
+import type {ICreatePetDto, IPetDto} from "entities/pet/model";
 
 export const petHandlers = [
     http.get('/api/all_pets', () => {
@@ -14,5 +15,22 @@ export const petHandlers = [
     }),
     http.get('/api/pet_by_id/:id', ({ params }) => {
         return HttpResponse.json(allPets.find(p => p.id === params.id));
-    })
+    }),
+    http.post('/api/add_new_pet', async (req) => {
+        const obj:ICreatePetDto = (await req.request.json()) as unknown as ICreatePetDto;
+        const newPet: IPetDto = {
+            id: '00' + obj.weight,
+            name: obj.name,
+            species: obj.species,
+            breed: obj.breed,
+            birthDate: obj.birthDate,
+            weight: obj.weight,
+            sex: obj.sex,
+        }
+        if (obj.photoUrl) newPet.photoUrl = obj.photoUrl;
+
+        allPets.push(newPet)
+        return HttpResponse.json(newPet)
+    }),
+
 ]
