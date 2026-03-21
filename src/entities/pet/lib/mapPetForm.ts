ee013675d4fce1adea979fdece320e65f9b7dba8
iamogir <ia.mogirevsky@gmail.com>
2026-03-21 18:35:27 +0200

    updated pet mapping

--- src/entities/pet/lib/mapPetForm.ts
+++ src/entities/pet/lib/mapPetForm.ts
@@ -1,18 +1,28 @@
-import type {IPetForm, ICreatePetDto} from "entities/pet/model";
+import type {IPetForm, ICreatePetDto, IUpdatedPetDto} from "entities/pet/model";
 import {parsePetBreed, parsePetSex, parsePetSpecies} from "entities/pet/lib";
 
-
-export const toServerPetObject = (obj: IPetForm): ICreatePetDto => {
-    const newObj: ICreatePetDto = {
+export const toServerPetObjectCreate = (obj: IPetForm): ICreatePetDto => {
+    const newObj : ICreatePetDto = {
         name: obj.name,
         species: parsePetSpecies(obj.species),
         breed: parsePetBreed(obj.breed),
-        birthDate: obj.birthDate,
+        birthDate: obj.birthDate.toString(),
         weight: obj.weight,
         sex: parsePetSex(obj.sex),
-        confirm: true
+        confirm: true,
     }
 
     if (obj.photoUrl) newObj.photoUrl = obj.photoUrl;
     return newObj;
+}
+
+export const toServerPetObjectUpdate = (id: string, obj: IPetForm): IUpdatedPetDto => {
+
+    const newObj = toServerPetObjectCreate(obj);
+    return {
+        id: id,
+        ...newObj,
+        photoUrl: obj.photoUrl ?? ''
+    };
+
 }
