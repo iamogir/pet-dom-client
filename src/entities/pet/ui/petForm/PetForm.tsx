b702572224eb0855a61e97f4f50d0a9ee5e5d5e8
iamogir <ia.mogirevsky@gmail.com>
2026-03-21 16:03:14 +0200

    handle change form

--- src/entities/pet/ui/petForm/PetForm.tsx
+++ src/entities/pet/ui/petForm/PetForm.tsx
@@ -1,67 +1,72 @@
-import {useEditPet} from "entities/pet/hooks";
+import {useAddNewPet, useEditPet} from "entities/pet/hooks";
 import style from "pages/addNewPetPage/ui/addNewPetPage.module.css";
-import {useEffect, useState} from "react";
 import {
     imagePlaceholder,
     type IPet,
     type IPetForm,
 } from "entities/pet/model";
+import {type ChangeEvent, useState} from "react";
 
 interface Props {
-    pet: IPet;
+    pet?: IPet;
 }
 
 export const PetForm = ({ pet }: Props) => {
 
-    const { mutate } = useEditPet();
-    const defaultValues: IPetForm = {
-        name: '',
-        species: '',
-        breed: '',
-        birthDate: '',
-        weight: -1,
-        sex: '',
-        photoUrl: imagePlaceholder,
+    const editPet = useEditPet();
+    const addPet = useAddNewPet();
+    const [form, setForm] = useState<IPetForm>({
+        name: pet?.name ?? '',
+        species: pet?.species?? '',
+        breed: pet?.breed ?? '',
+        birthDate: pet?.birthDate.toString() ?? '',
+        weight: pet?.weight ?? -1,
+        sex: pet?.sex ?? '',
+        photoUrl: pet?.photoUrl ?? imagePlaceholder,
         confirm: false
-    };
-    const values = !pet ? defaultValues : {
-        ...pet,
-        confirm: true
+    });
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        event.preventDefault();
+
+        const eventTarget = event.target;
+        setForm(prev => ({...prev, [eventTarget.name]: eventTarget.value}));
     }
 
+    const handleSubmit = (event) => {
 
+    }
 
     return (
         <div>
             <form className={style.box} onSubmit={handleSubmit}>
                 <label htmlFor={'name'}>Name: </label>
-                <input type={'text'} name={'name'} onChange={handleChange} value={values.name} />
+                <input type={'text'} name={'name'} onChange={handleChange} value={form.name} placeholder={'Name'} />
 
-                            <label htmlFor={'species'}>Species: </label>
-                            <input type={'text'} name={'species'} onChange={handleChange} value={values.species} />
+                <label htmlFor={'species'}>Species: </label>
+                <input type={'text'} name={'species'} onChange={handleChange} value={form.species} placeholder={'Species'} />
 
-                            <label htmlFor={'breed'}>Breed: </label>
-                            <input type={'text'} name={'breed'} onChange={handleChange} value={values.breed} />
+                <label htmlFor={'breed'}>Breed: </label>
+                <input type={'text'} name={'breed'} onChange={handleChange} value={form.breed} placeholder={'Breed'} />
 
-                            <label htmlFor={'birthDate'}>Birth date: </label>
-                            <input type={'date'} name={'birthDate'} onChange={handleChange} value={values.birthDate.toString()} />
+                <label htmlFor={'birthDate'}>Birth date: </label>
+                <input type={'date'} name={'birthDate'} onChange={handleChange} value={form.birthDate} placeholder={'Birth date'} />
 
-                            <label htmlFor={'sex'}>Sex: </label>
-                            <input type={'text'} name={'sex'} onChange={handleChange} value={values.sex} />
+                <label htmlFor={'sex'}>Sex: </label>
+                <input type={'text'} name={'sex'} onChange={handleChange} value={form.sex} placeholder={'Sex'} />
 
-                            <label htmlFor={'weight'}>Weight: </label>
-                            <input type={'number'} name={'weight'} onChange={handleChange} value={values.weight} />
+                <label htmlFor={'weight'}>Weight: </label>
+                <input type={'number'} name={'weight'} onChange={handleChange} value={form.weight} placeholder={'Weight'} />
 
-                            <label htmlFor={'photoUrl'}>Pet photo: </label>
-                            <input type={'text'} name={'photoUrl'} onChange={handleChange} value={values.photoUrl} />
+                <label htmlFor={'photoUrl'}>Pet photo: </label>
+                <input type={'text'} name={'photoUrl'} onChange={handleChange} value={form.photoUrl} placeholder={'Photo'} />
 
-                            <br/>
-                            <button>
-                                <label htmlFor={'submit'}>Add new pet</label>
-                                <input type={'submit'} name={'submit'} style={{display: 'none'}}/>
-                            </button>
-                        </form>
-
+                <br/>
+                <button>
+                    <label htmlFor={'submit'}>Add new pet</label>
+                    <input type={'submit'} name={'submit'} style={{display: 'none'}}/>
+                </button>
+            </form>
         </div>
     );
 };
