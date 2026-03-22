16bb44b55995a852a0673363fca8467357ed57e8
iamogir <ia.mogirevsky@gmail.com>
2026-03-22 20:44:32 +0200

    edited url

--- src/entities/pet/api/editPet.ts
+++ src/entities/pet/api/editPet.ts
@@ -3,7 +3,7 @@ import type {IPetDto, IUpdatedPetDto} from "entities/pet/model";
 import {fromServerPetObject} from "entities/pet/lib";
 
 export const editPet = async (data: IUpdatedPetDto) => {
-    const response: IPetDto = await fetchClient('/api/edit_pet/' + data.id, {method: 'PATCH', body: JSON.stringify(data)});
+    const response: IPetDto = await fetchClient('/edit_pet/' + data.id, {method: 'PATCH', body: JSON.stringify(data)});
     return fromServerPetObject(response);
 
 }
