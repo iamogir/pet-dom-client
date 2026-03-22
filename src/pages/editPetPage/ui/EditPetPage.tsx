80c7dbd340d5c8d2ffe0f8ae05539ce5d7a09e52
iamogir <ia.mogirevsky@gmail.com>
2026-03-22 20:19:22 +0200

    right editing pet page

--- src/pages/editPetPage/ui/EditPetPage.tsx
+++ src/pages/editPetPage/ui/EditPetPage.tsx
@@ -1,18 +1,16 @@
 import {PetForm} from "entities/pet/ui/petForm";
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {usePetById} from "entities/pet/hooks";
 
 export const EditPetPage = () => {
 
     const { id } = useParams();
-    const navigate = useNavigate();
     const { data } = usePetById(id ?? '');
-    if (!data) navigate('/')
 
     return (
         <div>
             <h1>What do you want to edit?</h1>
-            <PetForm pet={data} />
+            {data && <PetForm pet={data} /> }
         </div>
     );
 };
