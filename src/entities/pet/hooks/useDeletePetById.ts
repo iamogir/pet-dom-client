0ebbb546ffda16cf2fe2815d11ad7fbbf753d28b
iamogir <ia.mogirevsky@gmail.com>
2026-03-24 14:20:01 +0200

    use delete with optimistic cash

--- src/entities/pet/hooks/useDeletePetById.ts
+++ src/entities/pet/hooks/useDeletePetById.ts
@@ -1,9 +1,26 @@
-import {useMutation, type UseMutationOptions} from "@tanstack/react-query";
-import {deletePetById} from "entities/pet/api";
-import type {IPet} from "entities/pet/model";
+import {useMutation, type UseMutationOptions, useQueryClient} from "@tanstack/react-query";
+import {deletePetById, petQueryKeys} from "entities/pet/api";
+import type {IPet, IPetDto} from "entities/pet/model";
 
-export const useDeletePetById = (options?: UseMutationOptions<IPet, Error, string>) => {
+interface IContext {
+    prev?: IPetDto[];
+}
+
+export const useDeletePetById = (options?: UseMutationOptions<IPet, Error, string, IContext>) => {
+
+    const queryClient = useQueryClient();
+
     return useMutation({
+        onMutate: async (petId): Promise<IContext> => {
+            await queryClient.cancelQueries({ queryKey: petQueryKeys.all });
+            const prev = queryClient.getQueryData<IPetDto[]>(petQueryKeys.all);
+
+            queryClient.setQueryData(petQueryKeys.all, (old: IPetDto[] = []) => old.filter(p => p.id !== petId));
+            return { prev };
+        },
+        onError: (_error, _petId, context) => {
+            queryClient.setQueryData(petQueryKeys.all, context?.prev);
+        },
         mutationFn: deletePetById,
         ...options
     })
