5b50c38e4fd25f61cd32360ec8107848e97bdf7a
iamogir <ia.mogirevsky@gmail.com>
2026-03-24 19:49:26 +0200

    link to edit

--- src/entities/user/ui/userCard/UserCard.tsx
+++ src/entities/user/ui/userCard/UserCard.tsx
@@ -11,19 +11,24 @@ export const UserCard = ( { user }: Props ) => {
     const age = new Date().getFullYear() - user.birthDate.getFullYear();
 
     return (
-        <Link to={'/user/' + user.id}>
+
         <div className={style.box}>
-            <h3>{user.firstName} {user.lastName}</h3>
-            <p>{user.country}</p>
-            <p>{user.email}</p>
-            <p>{user.gender}</p>
-            <p>{user.password}</p>
-            <p>{user.phoneNumber}</p>
-            <p>age: {age}</p>
-            <div className={style.boxImage}>
-                <img src={user.avatarUrl} alt={user.firstName + ' photocard'} />
-            </div>
+            <Link to={'/user/' + user.id}>
+                <h3>{user.firstName} {user.lastName}</h3>
+                <p>{user.country}</p>
+                <p>{user.email}</p>
+                <p>{user.gender}</p>
+                <p>{user.password}</p>
+                <p>{user.phoneNumber}</p>
+                <p>age: {age}</p>
+                <div className={style.boxImage}>
+                    <img src={user.avatarUrl} alt={user.firstName + ' photocard'} />
+                </div>
+            </Link>
+            <Link to={'/edit_user/' + user.id}><button>edit profile</button></Link>
         </div>
-        </Link>
+
+
+
     );
 };
