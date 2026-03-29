886b0b4e515c1e6fcb13e2325b94c91f0cf6c52b
iamogir <ia.mogirevsky@gmail.com>
2026-03-29 22:12:39 +0300

    draft login btn

--- src/pages/homePage/ui/HomePage.tsx
+++ src/pages/homePage/ui/HomePage.tsx
@@ -3,14 +3,26 @@ import style from './homePage.module.css'
 import {WelcomePart} from "shared/ui/welcomePart";
 import {PetCard} from "entities/pet/ui/petCard";
 import {useAllPetsByUserId} from "entities/petOwner/hooks";
+import {useLogin} from "features/auth/hooks";
 
 export const HomePage = () => {
 
     const tempUserId = '00';
     const { data, error, isLoading } = useAllPetsByUserId(tempUserId);
+    const { mutateAsync } = useLogin();
+
+    const handleLogin = async () => {
+        const res = await mutateAsync({
+            email: 'email',
+            password: 'password',
+        })
+
+        console.log(res)
+    }
 
     return (
         <div>
+            <button onClick={handleLogin}>l o g i n</button>
             <WelcomePart/>
             <h3>Please, check your pets and their comfort:</h3>
             <br/>
