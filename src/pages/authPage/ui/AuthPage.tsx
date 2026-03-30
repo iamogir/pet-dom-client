f9726b05312992dee4d31ae325c56e16cb271be5
iamogir <ia.mogirevsky@gmail.com>
2026-03-30 21:48:23 +0300

    added handle

--- src/pages/authPage/ui/AuthPage.tsx
+++ src/pages/authPage/ui/AuthPage.tsx
@@ -2,12 +2,17 @@ import {Link} from "react-router-dom";
 import style from './authPage.module.css'
 
 export const AuthPage = () => {
+
+    const handleLogIn = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <div className={style.box}>
             <p>Welcome, please, sign in to your account or <Link to={'/sign_up'}><span>sign up</span></Link></p>
             <div className={style.content}>
                 <h2>SIGN IN</h2>
-                <form>
+                <form onSubmit={handleLogIn}>
                     <label htmlFor={'login'}>Login:
                     <input type={'email'} name={'login'} placeholder={'abc@mail.com'} /></label>
 
