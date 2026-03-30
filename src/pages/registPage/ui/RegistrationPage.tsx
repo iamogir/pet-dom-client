ca0a11b5882a523421088e157d0a2186c5117650
iamogir <ia.mogirevsky@gmail.com>
2026-03-30 21:57:04 +0300

    added input name

--- src/pages/registPage/ui/RegistrationPage.tsx
+++ src/pages/registPage/ui/RegistrationPage.tsx
@@ -13,10 +13,14 @@ export const RegistrationPage = () => {
             <div className={style.content}>
                 <h2>SIGN UP</h2>
                 <form onSubmit={handleSignUp}>
+
+                    <label htmlFor={'name'}>name:
+                        <input type={'text'} name={'name'} placeholder={'Zina'} /></label>
+
                     <label htmlFor={'email'}>e-mail:
                         <input type={'email'} name={'email'} placeholder={'abc@mail.com'} /></label>
 
-                    <label htmlFor={'password'}>Password:
+                    <label htmlFor={'password'}>password:
                         <input type={'password'} name={'password'} placeholder={'*****'} /></label>
 
                     <button type="submit" className={style.btn}>Sign up</button>
