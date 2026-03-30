9de2d1fdf1aa6376d134b40e868fff702eb25a2d
iamogir <ia.mogirevsky@gmail.com>
2026-03-30 13:30:38 +0300

    auth provider

--- src/features/auth/context/AuthProvider.tsx
+++ src/features/auth/context/AuthProvider.tsx
@@ -1,7 +1,20 @@
-export const AuthProvider = () => {
-    return (
-        <div>
+import {createContext, useState} from "react";
+import type {IAuthContext, IUserCurrent} from "features/auth/types";
+import * as React from "react";
 
-        </div>
+interface Props {
+    children: React.ReactNode;
+}
+
+const AuthContext = createContext<IAuthContext | null>(null);
+
+export const AuthProvider = ({children}: Props) => {
+
+    const [user, setUser] = useState<IUserCurrent | null>(null);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthContext.Provider>
     );
 };
