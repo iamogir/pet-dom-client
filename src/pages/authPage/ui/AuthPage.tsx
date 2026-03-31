bb4a3e98508118d146200942373d78be59a52e33
iamogir <ia.mogirevsky@gmail.com>
2026-03-31 12:33:27 +0300

    filling sign in form

--- src/pages/authPage/ui/AuthPage.tsx
+++ src/pages/authPage/ui/AuthPage.tsx
@@ -5,6 +5,7 @@ import {useState} from "react";
 
 export const AuthPage = () => {
 
+
     const [form, setForm] = useState<ILogin>({
         email: '',
         password: '',
@@ -12,10 +13,17 @@ export const AuthPage = () => {
 
     const handleChange = (event) => {
         event.preventDefault()
+
+        setForm(prev => ({ ...prev, [event.target.name]: event.target.value }))
+
     }
+
+    console.log(form)
 
     const handleLogIn = (event) => {
         event.preventDefault()
+
+
     }
 
     return (
@@ -24,11 +32,11 @@ export const AuthPage = () => {
             <div className={style.content}>
                 <h2>SIGN IN</h2>
                 <form onSubmit={handleLogIn}>
-                    <label htmlFor={'login'}>Login:
-                    <input type={'email'} name={'login'} value={form.email} placeholder={'abc@mail.com'} /></label>
+                    <label htmlFor={'email'}>Login:
+                    <input type={'email'} name={'email'} onChange={handleChange} value={form.email} placeholder={'abc@mail.com'} /></label>
 
                     <label htmlFor={'password'}>Password:
-                    <input type={'password'} name={'password'} value={form.password} placeholder={'*****'} /></label>
+                    <input type={'password'} name={'password'} onChange={handleChange} value={form.password} placeholder={'*****'} /></label>
 
                     <button type="submit" className={style.btn}>Log in</button>
                 </form>
