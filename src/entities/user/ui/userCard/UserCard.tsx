76c093daa3e4a6fd5d56d33f62814f2c678746d1
iamogir <ia.mogirevsky@gmail.com>
2026-04-03 22:20:07 +0300

    profile card style

--- src/entities/user/ui/userCard/UserCard.tsx
+++ src/entities/user/ui/userCard/UserCard.tsx
@@ -1,6 +1,6 @@
 import type {IUser} from "entities/user/model";
 import style from './userCard.module.css'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 interface Props {
     user: IUser
@@ -9,24 +9,29 @@ interface Props {
 export const UserCard = ( { user }: Props ) => {
 
     const age = new Date().getFullYear() - user.birthDate.getFullYear();
+    const navigate = useNavigate();
 
     return (
 
-        <div className={style.box}>
+        <article className={style.box}>
             <Link to={'/user/' + user.id}>
-                <h3>{user.firstName} {user.lastName}</h3>
-                <p>{user.country}</p>
-                <p>{user.email}</p>
-                <p>{user.gender}</p>
-                <p>{user.password}</p>
-                <p>{user.phoneNumber}</p>
-                <p>age: {age}</p>
-                <div className={style.boxImage}>
-                    <img src={user.avatarUrl} alt={user.firstName + ' photocard'} />
-                </div>
+                <ul>
+                    <li>{user.country}</li>
+                    <li>{user.gender}</li>
+                    <li>{user.phoneNumber}</li>
+                    <li>age: {age}</li>
+                </ul>
             </Link>
-            <Link to={'/edit_user/' + user.id}><button>edit profile</button></Link>
-        </div>
+            <button onClick={() => navigate('/edit_user/' + user.id)}>edit profile</button>
+            <section>
+                <h3>{user.firstName} {user.lastName}</h3>
+                <p>{user.email}</p>
+                <div className={style.boxImage}>
+                    <img src={user.avatarUrl} alt={user.firstName + ' photocard'}/>
+                </div>
+            </section>
+
+        </article>
 
 
 
