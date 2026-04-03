800f88567e7551eea08be1ee2db1c3e6c186080a
iamogir <ia.mogirevsky@gmail.com>
2026-04-03 21:57:24 +0300

    right tags

--- src/pages/profilePage/ui/ProfilePage.tsx
+++ src/pages/profilePage/ui/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {useUserById} from "entities/user/hooks";
 import {UserCard} from "entities/user/ui/userCard";
 import {PetCard} from "entities/pet/ui/petCard";
@@ -13,27 +13,31 @@ export const ProfilePage = ({ id }: Props) => {
 
     const { isLoading, data, error } = useUserById(id);
     const petsByUserQuery = useAllPetsByUserId(id);
+    const navigate = useNavigate();
 
     return (
         <div>
-            <h2>Welcome to your profile!</h2>
-            <h3>Check pets, sent tasks to family members and keep pet health excellent!</h3>
+            <section className={style.upper}>
+                <h2>Welcome to your profile!</h2>
+                <h3>Check pets, sent tasks to family members and keep pet health excellent!</h3>
+            </section>
             {isLoading ? <p>Loading...</p> :
                 (error ? <p>Oh, where is the data? Amiss! - {error.message}</p> :
-                    <div>
+                    <section className={style.cards}>
                         {data ? <UserCard key={data.id} user={data} /> : null }
 
-                        <h2>PETS BY USER {id} --- Or <Link to={'/add_pet'}><button>Add new pet</button></Link></h2>
+                        <h2>PETS BY USER {data?.firstName} --- Want to
+                            <button onClick={() => navigate('/add_pet')}>add new pet</button>
+                            ?
+                        </h2>
                         { petsByUserQuery.isLoading ? <p>loading...</p> :
                             (!petsByUserQuery.data ? <p>No data</p> :
                                 petsByUserQuery.data.data.map(p => <PetCard key={p.id} pet={p} />)
                             )
                         }
-                    </div>
+                    </section>
                 )
             }
-
-
         </div>
     );
 };
