12d111c257bf04eb1c4eb536ac42b5f4f358bf44
iamogir <ia.mogirevsky@gmail.com>
2026-04-06 22:12:43 +0300

    common breed filter

--- src/pages/petListPage/ui/PetFilter.tsx
+++ src/pages/petListPage/ui/PetFilter.tsx
@@ -1,21 +1,21 @@
-import {petSpecies} from "entities/pet/model";
+import {petBreed, petSpecies} from "entities/pet/model";
 import {useAllPets} from "entities/pet/hooks";
 import {PetCard} from "entities/pet/ui/petCard";
 
 interface Props {
     petType?: string,
-    petBreed?: string,
+    petBreeds?: string,
     filterFn: (event: any) => void
 }
 
-export const PetFilter = ({ petType, petBreed, filterFn }: Props) => {
+export const PetFilter = ({ petType, petBreeds, filterFn }: Props) => {
 
     const petsQuery = useAllPets();
 
 
     const filterResults = petsQuery.data?.data.filter(el => {
         const isType = petType ? el.species === petType : true;
-        const isBreed = petBreed ? el.breed === petBreed : true;
+        const isBreed = petBreeds ? el.breed === petBreeds : true;
 
         return isType && isBreed;
     })
@@ -26,6 +26,12 @@ export const PetFilter = ({ petType, petBreed, filterFn }: Props) => {
             <select value={petType} name={'type'} onChange={filterFn}>
                 <option value="">all pet types</option>
                 {petSpecies.map(sp => <option key={sp}>{sp}</option>)}
+            </select>
+
+            <p>Breed filter</p>
+            <select value={petBreeds} name={'breed'} onChange={filterFn}>
+                <option value="">all breeds</option>
+                {petBreed.map(br => <option key={br}>{br}</option>)}
             </select>
 
             { filterResults?.map(el => <PetCard key={el.id} pet={el}/>)}
