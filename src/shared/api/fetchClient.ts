118bf7a29464f4dbbe8efd35ead2c66afd964c93
iamogir <ia.mogirevsky@gmail.com>
2026-04-09 16:22:15 +0300

    updated fetch client

--- src/shared/api/fetchClient.ts
+++ src/shared/api/fetchClient.ts
@@ -1,13 +1,22 @@
 import {BASE_URL} from "shared/const";
+import {getToken} from "features/auth/utils";
 
 
 export const fetchClient: <T>(endpoint: string, options?: RequestInit) => Promise<T> =
     async (endpoint, options?)=> {
+
+    const token = getToken();
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json',
+        ...(options?.headers as Record<string, string>),
+    };
+
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
     const response = await fetch(BASE_URL + endpoint, {
-        headers: {
-            'Content-Type': 'application/json',
-            ...options?.headers,
-        },
+        ...headers,
         ...options,
     })
     if (!response.ok) {
