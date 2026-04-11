bbef4dfe89ade9de7d77f8de5acc50329b452062
iamogir <ia.mogirevsky@gmail.com>
2026-04-11 17:23:23 +0300

    get my pets works

--- src/entities/pet/api/getMyPets.ts
+++ src/entities/pet/api/getMyPets.ts
@@ -3,6 +3,6 @@ import {fromServerArrayPetsObject} from "entities/pet/lib";
 import type {IPetsDto} from "entities/pet/model";
 
 export const getMyPets = async () => {
-    const response: IPetsDto = await fetchClient('/user/me/pets');
+    const response: IPetsDto = await fetchClient('user/me/pets');
     return fromServerArrayPetsObject(response);
 }
