0d9d94fb5cb0f0ea0aaaa92ea4fe72e0bf36589b
iamogir <ia.mogirevsky@gmail.com>
2026-04-11 16:54:51 +0300

    access layer

--- src/entities/petOwner/api/getAllPetsByUserId.ts
+++ src/entities/petOwner/api/getAllPetsByUserId.ts
@@ -3,6 +3,6 @@ import {fetchClient} from "shared/api";
 import {fromServerArrayPetsObject} from "entities/pet/lib";
 
 export const getAllPetsByUserId = async (id: string) => {
-    const response: IPetsDto = await fetchClient('/all_pets_by_owner_id/' + id);
+    const response: IPetsDto = await fetchClient('/user/' + id + '/pets');
     return fromServerArrayPetsObject(response);
 }
