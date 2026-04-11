b2f1cb20ab7920d0adf79e8a88f9a09f51f573e5
iamogir <ia.mogirevsky@gmail.com>
2026-04-11 17:04:50 +0300

    added my pets getting

---
+++ src/entities/pet/api/getMyPets.ts
@@ -0,0 +1,8 @@
+import {fetchClient} from "shared/api";
+import {fromServerArrayPetsObject} from "entities/pet/lib";
+import type {IPetsDto} from "entities/pet/model";
+
+export const getMyPets = async () => {
+    const response: IPetsDto = await fetchClient('/user/me/pets');
+    return fromServerArrayPetsObject(response);
+}
--- src/entities/pet/hooks/useMyPets.ts
+++ src/entities/pet/hooks/useMyPets.ts
@@ -1,10 +1,14 @@
-import {useAllPetsByUserId} from "entities/petOwner/hooks";
 import {useMe} from "features/auth/hooks";
+import {useQuery} from "@tanstack/react-query";
+import {getMyPets} from "entities/pet/api/getMyPets.ts";
+import {petQueryKeys} from "entities/pet/api";
 
 export const useMyPets = () => {
     const { data } = useMe();
 
-    return useAllPetsByUserId(data?.id, {
+    return useQuery({
+        queryKey: petQueryKeys.list(data!.id),
+        queryFn: getMyPets,
         enabled: !!data,
     })
 }
