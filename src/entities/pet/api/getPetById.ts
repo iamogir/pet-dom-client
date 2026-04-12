76ec5d39d2256603cb377be9cbe26ff0739a33d0
iamogir <ia.mogirevsky@gmail.com>
2026-04-12 22:54:15 +0300

    getting pet by id works

--- src/entities/pet/api/getPetById.ts
+++ src/entities/pet/api/getPetById.ts
@@ -3,6 +3,6 @@ import type {IPetDto} from "entities/pet/model";
 import {fromServerPetObject} from "entities/pet/lib";
 
 export const getPetById = async (id: string) => {
-    const response: IPetDto = await fetchClient('/pet_by_id/' + id);
+    const response: IPetDto = await fetchClient('pet/' + id);
     return fromServerPetObject(response);
 }
