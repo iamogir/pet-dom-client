0d1135230a4232b9474d9d32a692579d3dbf0c18
iamogir <ia.mogirevsky@gmail.com>
2026-04-21 16:10:00 +0300

    consts for pet

---
+++ src/entities/pet/lib/const.ts
@@ -0,0 +1,27 @@
+export const petSex = [ 'male', 'female' ];
+
+// const petSpecies = [ 'cat', 'dog', 'hamster', 'rabbit', 'hare' ];
+const catBreed = [ 'cheerio', 'munchkin'];
+const dogBreed = [ 'corgi', 'beagle', 'spaniel'];
+export const petSpecies = [
+    {
+        name: 'cat',
+        breeds: catBreed
+    },
+    {
+        name: 'dog',
+        breeds: dogBreed
+    },
+    {
+        name: 'hamster',
+        breeds: [ 'somebody', 'smth']
+    },
+    {
+        name: 'rabbit',
+        breeds: ['black', 'white']
+    },
+    {
+        name: 'hare',
+        breeds: [ 'breed', 'hare breed']
+    }
+]
