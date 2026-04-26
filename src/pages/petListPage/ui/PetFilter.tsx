0740c8d6d578dab840f6fab7db877d6ee970e82f
iamogir <ia.mogirevsky@gmail.com>
2026-04-26 23:00:44 +0300

    deleted unused

--- src/pages/petListPage/ui/PetFilter.tsx
+++ src/pages/petListPage/ui/PetFilter.tsx
@@ -1,5 +1,5 @@
-import {birdBreed, petType} from "entities/pet/model";
 import type {ChangeEvent} from "react";
+import {petBreedMap} from "entities/pet/model";
 
 interface Props {
     petType: string,
@@ -14,13 +14,13 @@ export const PetFilter = ({ petType, petBreeds, filterFn }: Props) => {
             <p> Type filter</p>
             <select value={petType} name={'type'} onChange={filterFn}>
                 <option value="">all pet types</option>
-                {petType.map(sp => <option key={sp}>{sp}</option>)}
+                {Object.keys(petBreedMap).map(sp => <option key={sp}>{sp}</option>)}
             </select>
 
             <p>Breed filter</p>
             <select value={petBreeds} name={'breed'} onChange={filterFn}>
                 <option value="">all breeds</option>
-                {birdBreed.map(br => <option key={br}>{br}</option>)}
+                {petBreeds.map(br => <option key={br}>{br}</option>)}
             </select>
 
         </section>
