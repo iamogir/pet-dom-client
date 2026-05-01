5d2061ce0c8ad51c83fdbb8352e2725b8834ab3b
iamogir <ia.mogirevsky@gmail.com>
2026-05-01 20:33:27 +0300

    deleted trash

--- src/pages/petListPage/ui/PetListPage.tsx
+++ src/pages/petListPage/ui/PetListPage.tsx
@@ -55,10 +55,6 @@ export const PetListPage = () => {
     //
     // }
 
-    //TODO common fn for handle here
-    //TODO check working
-    //check base and back
-
     const handleSearchFilter = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setSearchParams(prev => {
             const params = new URLSearchParams(prev);
