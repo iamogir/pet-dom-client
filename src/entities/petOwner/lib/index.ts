8ad3ee8fdb3538e5132929ff8dae2de49eb66a6b
iamogir <ia.mogirevsky@gmail.com>
2026-05-02 22:31:13 +0300

    pet owner indexes deleted

--- src/entities/petOwner/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { fromServerObjectPetOwners } from './mapPetOwner.ts'
-export { fromServerArrayPetOwners } from './mapPetOwner.ts'
-export { assertOwnerRole } from './guards.ts'
-export { parseOwnerRole } from './parsers.ts'
--- src/entities/petOwner/model/index.ts
+++
@@ -1,4 +0,0 @@
-export type { OwnerRole } from './consts.ts';
-export { ownerRole } from './consts.ts';
-
-// export type { IPetOwner, IPetOwnerDto, IPetOwners, IPetOwnersDto } from './types.ts'
