1b94f45797acb14f50575d0c0d34162b3dccb9be
iamogir <ia.mogirevsky@gmail.com>
2026-05-03 22:09:41 +0300

    removed token 401

--- src/shared/api/fetchClient.ts
+++ src/shared/api/fetchClient.ts
@@ -1,8 +1,9 @@
 import {BASE_URL} from "shared/const";
-import {getToken} from "features/auth/utils";
+import {getToken, removeToken} from "features/auth/utils";
 
 
 export const fetchClient: <T>(endpoint: string, options?: RequestInit) => Promise<T> =
+
     async (endpoint, options?)=> {
 
     const token = getToken();
@@ -19,10 +20,14 @@ export const fetchClient: <T>(endpoint: string, options?: RequestInit) => Promis
         ...options,
         headers,
     })
-    if (!response.ok) {
-        const error = await response.text()
-        throw new Error(error)
+    if (response.status === 401) {
+       removeToken();
     }
+
+    // if (!response.ok) {
+    //     const error = await response.text()
+    //     throw new Error(error)
+    // }
 
     return response.json();
 }
