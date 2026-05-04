c4e502fe4ad8683dac9248adff5068f641e96b48
iamogir <ia.mogirevsky@gmail.com>
2026-05-04 22:23:01 +0300

    added api client

---
+++ src/shared/api/apiClient.ts
@@ -0,0 +1,20 @@
+import {getToken, removeToken} from "features/auth/utils";
+import {fetchClient} from "shared/api/fetchClient.ts";
+
+export const apiClient = async <T>(endpoint: string, options?: RequestInit) => {
+    const token = getToken();
+
+    const response: Response = await fetchClient(endpoint, {
+        ...options,
+        headers: {
+            'Content-Type': 'application/json',
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
+            ...options?.headers
+        },
+    });
+
+    if (response.status === 401) {
+        removeToken();
+        throw new Error('Unauthorized');
+    }
+}
