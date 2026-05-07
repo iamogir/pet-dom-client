90cdc2a525fcd00ee1cfa6b2155731fd63af655d
iamogir <ia.mogirevsky@gmail.com>
2026-05-07 22:52:43 +0300

    temp added

--- src/features/weather/ui/Weather.tsx
+++ src/features/weather/ui/Weather.tsx
@@ -1,10 +1,9 @@
 import style from './weather.module.css'
-import {apiClient} from "shared/api";
 import {useEffect, useState} from "react";
 
 export const Weather = () => {
 
-    const [word, setWord] = useState('unknown weather');
+    const [weather, setWeather] = useState({temp: 0, state: 'unknown weather'});
     const url = 'https://api.weather.gov/points/40.7128,-74.0060';
 
     const fn = async () => {
@@ -14,23 +13,24 @@ export const Weather = () => {
         const forecastRes = await fetch(forecastUrl);
 
         const forecastData = await forecastRes.json();
+        const current = forecastData.properties.periods[0]
 
-        return forecastData.properties.periods[0].shortForecast;
+        return { temp: current.temperature, state: current.shortForecast };
     }
 
     useEffect(() => {
-        console.log(word)
+        console.log(weather);
         const fn2 = async () => {
             const res = await fn();
             console.log(res)
-            setWord(res);
+            setWeather(res);
         }
         fn2();
-    })
+    }, [])
 
     return (
         <>
-            {word.toLowerCase()}
+            {weather.state.toLowerCase()} ({weather.temp + 'F'})
         </>
     );
 };
