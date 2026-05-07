655789d148f34b23aeceb2274bdbf14e12df81de
iamogir <ia.mogirevsky@gmail.com>
2026-05-07 22:42:50 +0300

    fast getting weather now

--- src/features/weather/ui/Weather.tsx
+++ src/features/weather/ui/Weather.tsx
@@ -1,8 +1,10 @@
 import style from './weather.module.css'
 import {apiClient} from "shared/api";
+import {useEffect, useState} from "react";
 
 export const Weather = () => {
 
+    const [word, setWord] = useState('unknown weather');
     const url = 'https://api.weather.gov/points/40.7128,-74.0060';
 
     const fn = async () => {
@@ -13,16 +15,22 @@ export const Weather = () => {
 
         const forecastData = await forecastRes.json();
 
-        console.log(forecastData.properties.periods[0]);
-
-        return
+        return forecastData.properties.periods[0].shortForecast;
     }
 
-    fn();
+    useEffect(() => {
+        console.log(word)
+        const fn2 = async () => {
+            const res = await fn();
+            console.log(res)
+            setWord(res);
+        }
+        fn2();
+    })
 
     return (
-        <div>
-            weather
-        </div>
+        <>
+            {word.toLowerCase()}
+        </>
     );
 };
