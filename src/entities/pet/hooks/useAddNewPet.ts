f1630a2113530aa43868f58a9ad67f527bb83d74
iamogir <ia.mogirevsky@gmail.com>
2026-05-20 21:39:11 +0300

    optimistic update creating

--- src/entities/pet/hooks/useAddNewPet.ts
+++ src/entities/pet/hooks/useAddNewPet.ts
@@ -1,5 +1,7 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {addNewPet, petQueryKeys} from "entities/pet/api";
+import type {IPet} from "entities/pet/model";
+import * as crypto from "node:crypto";
 // import type {ICreatePetDto, IPet} from "entities/pet/model";
 
 export const useAddNewPet = (
@@ -10,9 +12,19 @@ export const useAddNewPet = (
 
     return useMutation({
         mutationFn: addNewPet,
-        // ...options,
+        onMutate: async (newPet) => {
+            await queryClient.cancelQueries({ queryKey: petQueryKeys.all});
+            const prevPets = queryClient.getQueryData<IPet[]>(petQueryKeys.all);
+            queryClient.setQueryData<IPet[]>(petQueryKeys.all,
+                (old = []): IPet[] => [
+                    ...old,
+                    {
+                        id: crypto.randomUUID(),
+                        ...newPet}
+                ]
+            )
+        },
         onSuccess: () => queryClient.invalidateQueries({ queryKey: petQueryKeys.all}),
-        onSettled: () => queryClient.invalidateQueries({ queryKey: petQueryKeys.all}),
         },
 
     )
