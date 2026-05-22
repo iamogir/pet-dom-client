b6728fe175c0bdee696d20f554eb4db7c91d1994
iamogir <ia.mogirevsky@gmail.com>
2026-05-22 13:16:01 +0300

    deleted comment

--- src/entities/pet/hooks/useAddNewPet.ts
+++ src/entities/pet/hooks/useAddNewPet.ts
@@ -36,7 +36,6 @@ export const useAddNewPet = (options?: UseMutationOptions<IPet, Error, ICreatePe
             queryClient.setQueryData(petQueryKeys.all, context?.prevPets)
 
         },
-        //add options
         onSuccess: () => queryClient.invalidateQueries({ queryKey: petQueryKeys.all}),
         },
 
