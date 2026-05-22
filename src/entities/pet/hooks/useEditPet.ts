d457bb3df385db5fa3f8e0442b6d265a8ec31ebc
iamogir <ia.mogirevsky@gmail.com>
2026-05-22 20:48:42 +0300

    optimistic update (update pet)

--- src/entities/pet/hooks/useEditPet.ts
+++ src/entities/pet/hooks/useEditPet.ts
@@ -1,6 +1,7 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {editPet, petQueryKeys} from "entities/pet/api";
-import type {IPet} from "entities/pet/model";
+import type {IPet, IPets, IUpdatedPetDto} from "entities/pet/model";
+import {fromServerPetObject} from "entities/pet/lib";
 
 export const useEditPet = () => {
 
@@ -13,6 +14,23 @@ export const useEditPet = () => {
         //         (old: IPet[] = []) => old.map(p => p.id === updatedPet.id ? updatedPet : p)
         //     )
         // }
+        onMutate: async (updatedPet: IUpdatedPetDto)=> {
+            await queryClient.cancelQueries({ queryKey: petQueryKeys.all });
+            const prevPets = queryClient.getQueryData<IPets>(petQueryKeys.all);
+
+            queryClient.setQueriesData({ queryKey: petQueryKeys.all }, (old: IPets = {data: [], meta: {total: 0}}): IPets => {
+                const saveOld = old ?? {data: [], meta: {total: 0}};
+                const newData = saveOld.data.map((el) => el.id === updatedPet.id ? fromServerPetObject(updatedPet) : el);
+
+                return {
+                    data: newData,
+                    meta: {total: newData.length}
+                }
+            })
+
+            return { prevPets };
+
+        },
         onSuccess: (updatedPet:IPet) => {
             queryClient.setQueryData(petQueryKeys.single(updatedPet.id),
                 // (old: IPet[] = []) => old.map(p => p.id === updatedPet.id ? updatedPet : p)
