b71ac6a2c1064311991318196088a83df793a361
iamogir <ia.mogirevsky@gmail.com>
2026-05-26 14:15:48 +0300

    scroll cards

--- src/pages/homePage/ui/HomePage.tsx
+++ src/pages/homePage/ui/HomePage.tsx
@@ -4,11 +4,46 @@ import {PetCard} from "entities/pet/ui/petCard";
 import {useMyPets} from "entities/pet/hooks";
 import {EmptyState} from "features/emptyState/ui";
 import {Loader} from "shared/ui/loader";
+import {useEffect, useRef, useState} from "react";
 
 export const HomePage = () => {
 
     const {data, isLoading, error} = useMyPets();
     // const temp = useAllPetsByUserId("292060dd-24dd-48e5-b2be-be1d6f2855cd");
+    const petBox = useRef(null);
+    const [showLeft, setShowLeft] = useState(false);
+    const [showRight, setShowRight] = useState(false);
+
+    const scrollAmount = 150;
+
+    const scrollBox = (value: number) => {
+        const box = petBox.current as unknown as HTMLElement;
+        if (box) {
+            box.scrollBy({
+                left: value,
+                behavior: "smooth"
+            })
+        }
+    }
+
+    const toggleButtons = () => {
+        const box = petBox.current as unknown as HTMLElement;
+        if (box) {
+            const { scrollLeft, scrollWidth, clientWidth } = box;
+            setShowLeft(scrollLeft > 1);
+            setShowRight(scrollLeft < scrollWidth - clientWidth - 1);
+        }
+
+    }
+
+    useEffect(() => {
+        toggleButtons();
+        window.addEventListener('resize', toggleButtons);
+
+        return () => {
+            window.removeEventListener('resize', toggleButtons);
+        };
+    }, [])
 
     return (
         <>
@@ -18,13 +53,15 @@ export const HomePage = () => {
             <h2>Please, check your pets and their comfort:</h2>
             {isLoading ? <Loader/> :
                 error ? <EmptyState variant={'pets'}/> :
-                    <section className={style.petCards}>
+                    <section className={style.petCards} ref={petBox} onScroll={toggleButtons}>
                         {data?.data.map((pet) => <PetCard key={pet.id} pet={pet}/>)}
                         <div>
                             Add new pet +
                         </div>
                     </section>
             }
+            { showLeft && <button onClick={() => scrollBox(-scrollAmount)}>LEFT</button>}
+            { showRight && <button onClick={() => scrollBox(scrollAmount)}>RIGHT</button>}
             <h1 style={{ color: 'red' }}>. . . ask ai . . .</h1>
         </>
     );
