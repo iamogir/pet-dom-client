a356ae8d733369dd3cc781f38373f1f940d0040f
iamogir <ia.mogirevsky@gmail.com>
2026-05-27 20:43:13 +0300

    shadows and icons

--- src/widgets/bottomMenu/ui/BottomMenu.tsx
+++ src/widgets/bottomMenu/ui/BottomMenu.tsx
@@ -1,5 +1,9 @@
 import {useNavigate} from "react-router-dom";
 import style from './bottomMenu.module.css'
+import Home from '../../../shared/assert/icons/home.svg?react'
+import Paw from '../../../shared/assert/icons/paw.svg?react'
+import Settings from '../../../shared/assert/icons/settings.svg?react'
+import Profile from '../../../shared/assert/icons/profile.svg?react'
 
 export const BottomMenu = () => {
 
@@ -7,10 +11,10 @@ export const BottomMenu = () => {
 
     return (
         <nav className={style.menu}>
-            <button onClick={() => navigate('/home')}>Home</button>
-            <button onClick={() => navigate('/my_pets')}>My pets</button>
-            <button onClick={() => navigate('/user/me')}>My profile</button>
-            <button onClick={() => navigate('#')}>Settings</button>
+            <button onClick={() => navigate('/home')}><Home/></button>
+            <button onClick={() => navigate('/my_pets')}><Paw/></button>
+            <button onClick={() => navigate('/user/me')}><Profile/></button>
+            <button onClick={() => navigate('#')}><Settings/></button>
         </nav>
     );
 };
