af6a529741c5e54fbc04dcbbfa1df33bbd2eaaed
iamogir <ia.mogirevsky@gmail.com>
2026-05-31 20:50:49 +0300

    dynamic cards

--- src/entities/pet/ui/petCard/PetCard.tsx
+++ src/entities/pet/ui/petCard/PetCard.tsx
@@ -1,9 +1,9 @@
 import {type IPet} from "entities/pet/model";
 import style from './petCard.module.css'
 import {Link, useNavigate} from "react-router-dom";
-import {useDeletePetById} from "entities/pet/hooks";
-import {useQueryClient} from "@tanstack/react-query";
-import {petQueryKeys} from "entities/pet/api";
+// import {useDeletePetById} from "entities/pet/hooks";
+// import {useQueryClient} from "@tanstack/react-query";
+// import {petQueryKeys} from "entities/pet/api";
 import {getPetAvatar} from "entities/pet/lib";
 import Paw from 'shared/assert/icons/paw-filled.svg?react'
 import Calendar from '../../../../shared/assert/icons/calendar.svg?react'
@@ -14,16 +14,19 @@ interface Props {
 
 export const PetCard = ({ pet }: Props) => {
 
-    const petAge = new Date().getFullYear() - pet.birthDate.getFullYear();
-    const bDay: string = String(pet.birthDate.getDate()).padStart(2, '0') + '/' + String(pet.birthDate.getMonth() + 1).padStart(2, '0') + '/' + pet.birthDate.getFullYear();
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
-    const { mutateAsync } = useDeletePetById(
-        {
-        onSuccess: () =>
-            queryClient.invalidateQueries({ queryKey: petQueryKeys.all})
-        }
-    );
+    const petAge = pet.birthDate ? new Date().getFullYear() - pet.birthDate.getFullYear() : null;
+
+    console.log(pet)
+
+    // const bDay: string = String(pet.birthDate.getDate()).padStart(2, '0') + '/' + String(pet.birthDate.getMonth() + 1).padStart(2, '0') + '/' + pet.birthDate.getFullYear();
+    // const queryClient = useQueryClient();
+    // const navigate = useNavigate();
+    // const { mutateAsync } = useDeletePetById(
+    //     {
+    //     onSuccess: () =>
+    //         queryClient.invalidateQueries({ queryKey: petQueryKeys.all})
+    //     }
+    // );
 
     // const handleDelete = async () => {
     //     const isConfirmed = confirm("Are you sure you want to delete this pet?");
@@ -46,12 +49,14 @@ export const PetCard = ({ pet }: Props) => {
                 <div className={style.info}>
                     <div>
                         <Paw className={style.icon}/>
-                        <p>{pet.breed}</p>
+                        {pet.breed ? <p>{pet.breed}</p> : <p>{pet.species}</p>}
                     </div>
-                    <div>
-                        <Calendar className={style.icon}/>
-                        <p>{petAge} years</p>
-                    </div>
+                    {petAge &&
+                        <div>
+                            <Calendar className={style.icon}/>
+                            <p>{petAge} years</p>
+                        </div>
+                    }
                 </div>
             </section>
         </Link>
