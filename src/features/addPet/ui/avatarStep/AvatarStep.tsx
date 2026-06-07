add9038753a4c3d00662f237d1abb329f974e132
iamogir <ia.mogirevsky@gmail.com>
2026-06-07 21:40:39 +0300

    just check up

--- src/features/addPet/ui/avatarStep/AvatarStep.tsx
+++ src/features/addPet/ui/avatarStep/AvatarStep.tsx
@@ -13,6 +13,8 @@ export const AvatarStep = ({ form, onSubmit, isPending }: Props) => {
             <label >He looks like:</label>
             <input type={'file'} />
 
+            //check up
+
             <Button onClick={form.prevStep} text={'Back'}/>
             <Button onClick={onSubmit} text={'Submit'} disabled={isPending}/>
         </div>
