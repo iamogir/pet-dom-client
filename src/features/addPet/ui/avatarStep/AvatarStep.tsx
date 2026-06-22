9f4d5e1bae7dd0cdb8ad48c7d0972b27aca1bfbc
iamogir <ia.mogirevsky@gmail.com>
2026-06-22 20:47:43 +0300

    temp preview

--- src/features/addPet/ui/avatarStep/AvatarStep.tsx
+++ src/features/addPet/ui/avatarStep/AvatarStep.tsx
@@ -8,12 +8,26 @@ interface Props {
 }
 
 export const AvatarStep = ({ form, onSubmit, isPending }: Props) => {
+
+    const canPreviewLocally = (file: File) => {
+
+        return ['image/jpeg', 'image/png', 'image/webp'].includes(file.type);
+
+    };
+
     return (
         <div>
             <label >He looks like:</label>
-            <input name={'avatar'} type={'file'}
-                   onChange={(e) => { if (e.target.files?.[0]) form.setFormFields(e.target.name as keyof FormData, e.target.files?.[0])}}
+            <input name={'avatar'} type={'file'} accept="image/jpeg,image/png,image/webp,image/heic,image/heif"
+                   onChange={(e) => {
+                       const file = e.target.files?.[0];
+                       if (!file) return;
+                       form.setFormFields(e.target.name as keyof FormData, file)
+                   }}
             />
+
+            {form.formData.avatar && canPreviewLocally(form.formData.avatar) ? (<img src={URL.createObjectURL(form.formData.avatar)} alt="Pet preview" />) :
+                form.formData.avatar ? ( <p>{form.formData.avatar.name}</p>) : null}
 
             <Button onClick={form.prevStep} text={'Back'}/>
             <Button onClick={onSubmit} text={'Submit'} disabled={isPending}/>
