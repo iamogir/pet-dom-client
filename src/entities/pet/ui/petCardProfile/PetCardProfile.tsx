cdd8dcb6db4d5e5016605de725fef16b3343f00b
iamogir <ia.mogirevsky@gmail.com>
2026-06-29 21:47:08 +0300

    tablet mode card

--- src/entities/pet/ui/petCardProfile/PetCardProfile.tsx
+++ src/entities/pet/ui/petCardProfile/PetCardProfile.tsx
@@ -17,29 +17,31 @@ export const PetCardProfile = ({ pet }: Props) => {
         <article className={style.container}>
             <img src={pet.photoUrl} alt={pet.name}/>
             <span>{pet.species.toUpperCase()}</span>
-            <section className={style.data}>
-                <p>{pet.name}</p>
-                <span>{pet.species.toUpperCase()}</span>
-                <p>
-                    <span>{pet.breed || ''}</span>
-                    <span>{petAge ? '•' : ''}</span>
-                    <span>{petAge ? petAge + ' y.o.' : ''}</span></p>
-            </section>
-            <section className={style.info}>
-                { pet.weight &&
-                    <div>
-                        <p>WEIGHT</p>
-                        <p>{pet.weight}</p>
-                    </div>
-                }
-                { pet.birthDate &&
-                    <div>
-                        <p>BIRTH DAY</p>
-                        <p>{pet.birthDate.toDateString()}</p>
-                    </div>
-                }
-            </section>
-            <Button onClick={() => navigate('/edit_pet/' + pet.id)} text={'Edit profile'} />
+            <div>
+                <section className={style.data}>
+                    <p>{pet.name}</p>
+                    <span>{pet.species.toUpperCase()}</span>
+                    <p>
+                        <span>{pet.breed || ''}</span>
+                        <span>{petAge ? '•' : ''}</span>
+                        <span>{petAge ? petAge + ' y.o.' : ''}</span></p>
+                </section>
+                <section className={style.info}>
+                    {pet.weight &&
+                        <div>
+                            <p>WEIGHT</p>
+                            <p>{pet.weight}</p>
+                        </div>
+                    }
+                    {pet.birthDate &&
+                        <div>
+                            <p>BIRTH DAY</p>
+                            <p>{pet.birthDate.toDateString()}</p>
+                        </div>
+                    }
+                </section>
+                <Button onClick={() => navigate('/edit_pet/' + pet.id)} text={'Edit profile'}/>
+            </div>
         </article>
     );
 };
