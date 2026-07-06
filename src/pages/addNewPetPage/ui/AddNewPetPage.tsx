4eb0eb62d2487cced23172e0c7bf37908cbcd2f5
iamogir <ia.mogirevsky@gmail.com>
2026-07-06 20:53:34 +0300

    manage window on steps

--- src/pages/addNewPetPage/ui/AddNewPetPage.tsx
+++ src/pages/addNewPetPage/ui/AddNewPetPage.tsx
@@ -4,11 +4,13 @@ import {AddPetForm} from "entities/pet/ui/addPetForm";
 export const AddNewPetPage = () => {
 
     return (
-        <div className={style.box}>
-            <h1>New Companion</h1>
-            <p>Let's create a profile for your new best friend.</p>
-            <p className={style.steps}> !! Progress dots !!</p>
-            <AddPetForm/>
+        <div className={style.container}>
+            <div className={style.box}>
+                <h1>New Companion</h1>
+                <p>Let's create a profile for your new best friend.</p>
+                <p className={style.steps}> !! Progress dots !!</p>
+                <AddPetForm/>
+            </div>
         </div>
     );
 };
