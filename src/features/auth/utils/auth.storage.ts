1ab9efc99e8c4cfcf8f02deaa3c42c9dd45be3ca
iamogir <ia.mogirevsky@gmail.com>
2026-07-16 20:37:04 +0300

    refactor: move token storage to shared api

--- src/features/auth/utils/auth.storage.ts
+++ src/features/auth/utils/auth.storage.ts
@@ -1,13 +1,1 @@
-const TOKEN_KEY = 'token';
-
-export const setToken = (token: string) => {
-    localStorage.setItem(TOKEN_KEY, token);
-}
-
-export const getToken = () => {
-    return localStorage.getItem(TOKEN_KEY);
-}
-
-export const removeToken = () => {
-    localStorage.removeItem(TOKEN_KEY);
-}
+export {setToken, getToken, removeToken} from "shared/api";
--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,6 +1,6 @@
-import {getToken} from "features/auth/utils";
 import {fetchClient} from './fetchClient.ts'
 import {ApiError, AuthError, ServerError, ValidationError} from './errors.ts';
+import {getToken} from './access.ts';
 
 export const apiClient = async <T>(endpoint: string, options?: RequestInit): Promise<T> => {
     const token = getToken();
