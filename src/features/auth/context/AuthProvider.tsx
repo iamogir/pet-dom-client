d0c22120c4a972a8d5fc13f4507dcd4e0ca1c3ad
iamogir <ia.mogirevsky@gmail.com>
2026-07-18 13:57:19 +0300

    chore: remove stale auth provider comments

--- src/features/auth/context/AuthProvider.tsx
+++ src/features/auth/context/AuthProvider.tsx
@@ -1,23 +1,12 @@
 import * as React from "react";
 import { AuthContext } from "./auth.context";
 import {logout} from "features/auth/service";
-// import {userQueryKeys} from "entities/user/api";
 
 interface Props {
     children: React.ReactNode;
 }
 
 export const AuthProvider = ({children}: Props) => {
-
-    // const queryClient = useQueryClient();
-
-    // const logout = () => {
-    //     removeToken();
-    //     // queryClient.removeQueries({ queryKey: userQueryKeys.me() });
-    //     // queryClient.removeQueries({ queryKey: ['my_pets']});
-    //     queryClient.clear();
-    //
-    // };
 
     return (
         <AuthContext.Provider value={ { logout }}>
