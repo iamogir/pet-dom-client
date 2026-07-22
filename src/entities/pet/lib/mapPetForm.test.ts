c0b10a9604699208a27159843efee2a6cd320db5
iamogir <ia.mogirevsky@gmail.com>
2026-07-22 22:03:51 +0300

    test: cover optional pet create fields

--- src/entities/pet/lib/mapPetForm.test.ts
+++ src/entities/pet/lib/mapPetForm.test.ts
@@ -1,15 +1,32 @@
 import {describe, expect, it} from "vitest";
+import {toServerPetObjectCreate} from "entities/pet/lib/mapPetForm.ts";
 
 describe('toServerPetObjectCreate', () => {
     it('should make form fields to DTO', () => {
-        const result = {
+        const result = toServerPetObjectCreate({
             name: 'Micky',
             species: 'dog'
-        }
+        })
 
         expect(result).toEqual({
             name: 'Micky',
             species: 'dog'
         })
+    });
+
+    it("add unrequired fields if filled", () => {
+        const result = toServerPetObjectCreate({
+            name: "Mika",
+            species: "dog",
+            breed: "corgi",
+            photoUrl: "https://example.com/mika.jpg",
+        });
+
+        expect(result).toEqual({
+            name: "Mika",
+            species: "dog",
+            breed: "corgi",
+            photoUrl: "https://example.com/mika.jpg",
+        });
     });
 })
